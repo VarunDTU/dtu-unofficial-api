6357c11d0e29e901d6061fea8c9fa27f2292e69c
Varun <varun.py10@gmail.com>
2023-02-11 19:58:00 +0530

    full user-info added

--- index.js
+++ index.js
@@ -78,13 +78,33 @@ async function get_user_info(id,password){
         await browser.close()
         return "wrong password or id"
     }
+    await page.screenshot({path:'p1.png'})
     await Promise.all([page.click("#Link145"),page.waitForNavigation()])
-    const dob=await page.$eval("#Label597",el=>el.textContent)
-    console.log(dob);
+    
+    await page.screenshot({path:'p2.png'})
+    var dob=await page.$eval("#Label597",el=>el.textContent)
+    dob=dob.split('\n').join(' ')
+    var Branch=await page.$eval("#Label613",el=>el.textContent)
+    Branch=Branch.split('\n').join(' ')
+    var email=await page.$eval("#Label240",el=>el.textContent)
+    email=email.split('\n').join(' ')
+    var Father_name=await page.$eval("#Label242",el=>el.textContent)
+    Father_name=Father_name.split('\n').join(' ')
+    var Mother_name=await page.$eval("#Label243",el=>el.textContent)
+    Mother_name=Mother_name.split('\n').join(' ')
+    var Phone=await page.$eval("#Label265",el=>el.textContent)
+    Phone=Phone.split('\n').join(' ')
+    var Address=await page.$eval("#Label596",el=>el.textContent)
+    Address=Address.split('\n').join(' ')
+    var Availing_Hostel=await page.$eval("#ListItem735",el=>el.textContent)
+    Availing_Hostel=Availing_Hostel.split('\n').join(' ')
+    console.log(dob,Branch,email,Father_name,Mother_name,Phone,Address,Availing_Hostel);
+    user_profile.push({id,dob,Branch,email,Father_name,Mother_name,Phone,Address,Availing_Hostel})
     
     
     await browser.close()
-    return dob;
+
+    return user_profile;
 }
 var request_notices=setInterval(() => {
     latest_news= web_scrapping('tab4');
