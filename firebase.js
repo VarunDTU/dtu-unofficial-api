e608f0ffc1dae2bdfa1c5b2044adb1d5f17808fb
Varun Negi <varun.py10@gmail.com>
2023-08-01 20:28:44 +0530

    1st time no data issue solved

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-import * as dotenv  from "dotenv"
-dotenv.config()
+import { getFirestore } from "firebase/firestore";
+import * as dotenv from "dotenv";
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,10 +13,10 @@ const firebaseConfig = {
   projectId: process.env.PROJECTID,
   storageBucket: process.env.STORAGEBUCKET,
   messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID
+  appId: process.env.APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db=getFirestore(app)
+export const db = getFirestore(app);
